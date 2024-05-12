9bddd3c00c9ea046b4adf7c257c883c10ab35460
Aryan <aryansharma31658@gmail.com>
2024-05-12 14:03:06 +0530

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HashLocationStrategy,LocationStrategy} from '@angular/common'
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
 
@@ -12,7 +12,7 @@ import { HttpClientModule } from  '@angular/common/http';
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide:LocationStrategy, useClass:HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
